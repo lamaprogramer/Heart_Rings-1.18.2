931d5749050b61cd28cbc6f56f0c93420221a1a6
BuildTools <unconfigured@null.spigotmc.org>
2022-07-24 15:20:42 -0400

    nbt stuff, and more recipes

--- src/main/java/net/iamaprogrammer/heartcanister/item/ModItems.java
+++ src/main/java/net/iamaprogrammer/heartcanister/item/ModItems.java
@@ -13,19 +13,19 @@ public class ModItems {
 
     // Heart Rings
     public static final Item RED_RING = registerItem("red_heart_ring",
-            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1), 4.0f));
+            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).maxDamage(2), 4.0f, 1));
 
     public static final Item BLUE_RING = registerItem("blue_heart_ring",
-            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1), 8.0f));
+            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).maxDamage(4), 8.0f, 2));
 
     public static final Item GREEN_RING = registerItem("green_heart_ring",
-            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1), 12.0f));
+            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).maxDamage(6), 12.0f, 3));
 
     public static final Item YELLOW_RING = registerItem("yellow_heart_ring",
-            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1), 16.0f));
+            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).maxDamage(8), 16.0f, 4));
 
     public static final Item PURPLE_RING = registerItem("purple_heart_ring",
-            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1), 20.0f));
+            new LifeRingItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).maxDamage(10), 20.0f,  5));
 
 
     // Life Shard
--- src/main/java/net/iamaprogrammer/heartcanister/util/ModLootTableModifiers.java
+++ src/main/java/net/iamaprogrammer/heartcanister/util/ModLootTableModifiers.java
@@ -14,6 +14,8 @@ import net.minecraft.util.Identifier;
 public class ModLootTableModifiers {
     private static final Identifier WITCH_ID
             = new Identifier("minecraft", "entities/witch");
+    private static final Identifier END_CITY_STRUCTURE_CHEST_ID
+            = new Identifier("minecraft", "chests/end_city_treasure");
     public static void modifyLootTables() {
         LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
 
@@ -21,7 +23,17 @@ public class ModLootTableModifiers {
                 // Adds a Life Shards to Witches.
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.01f)) // Drops 1% of the time
+                        .conditionally(RandomChanceLootCondition.builder(0.001f)) // Drops 0.1% of the time
+                        .with(ItemEntry.builder(ModItems.LIFE_SHARD))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+
+            if(END_CITY_STRUCTURE_CHEST_ID.equals(id)) {
+                // Adds a Life Shards to End City Chests.
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.05f)) // Drops 5% of the time
                         .with(ItemEntry.builder(ModItems.LIFE_SHARD))
                         .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 supplier.withPool(poolBuilder.build());
